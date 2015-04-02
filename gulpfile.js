5dc42435162037139c9a1bac4d7326738932c439
Borys_Ihnatyev <paur.link@gmail.com>
2015-04-02 11:26:14 +0300

    [Automation] - Added 'vendor' task for copy 'bower_components/*/dist/*.js' files to 'dist/vendor' folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
-    del = require('del');
+    del = require('del'),
+    rename = require('gulp-rename');
+
+//region src markup copy
 
 gulp.task('markup', ['clean:markup'], function () {
     return gulp.src('src/*.*')
@@ -9,3 +12,21 @@ gulp.task('markup', ['clean:markup'], function () {
 gulp.task('clean:markup', function (onDone) {
     del(['dist/*.*'], onDone);
 });
+
+//endregion
+
+//region vendor js copy
+
+gulp.task('vendor', ['clean:vendor'], function () {
+    return gulp.src('bower_components/*/dist/*.js')
+        .pipe(rename({
+            dirname: ""
+        }))
+        .pipe(gulp.dest('dist/vendor'));
+});
+
+gulp.task('clean:vendor', function (onDone) {
+    del(['dist/vendor/**/*.*'], onDone);
+});
+
+//endregion
