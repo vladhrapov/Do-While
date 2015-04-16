03d3b7f7df747ab431a316d72bd3691a522dcb85
Borys Ihnatyev <paur.link@gmail.com>
2015-04-16 23:27:11 +0300

    [Bootstrap] - Removed comment

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-//todo: remove this route, its not useful and created only for test
 router.get('/', function(req, res, next) {
   res.json({
     text : 'hello world!',
