9e329fb5c40e2d0c89adbe8058128ea0b0278e9a
Borys Ihnatyev <paur.link@gmail.com>
2015-04-24 00:44:47 +0300

    [Automation] - Added patterns relativity

--- gulp/lib/glob.js
+++ gulp/lib/glob.js
@@ -1,50 +1,59 @@
 var path = require('path');
 
-var Builder = function (pattern, ext) {
+const CURRENT_DIR = './';
+const PATTERN_ALL = '*';
+
+var Builder = function(pattern, ext) {
     this._isExclude = false;
     this._pattern = pattern;
 
     if (typeof ext === 'undefined')
-        ext = '*';
+        ext = PATTERN_ALL;
 
     this._extension = ext;
 };
 
-Builder.prototype.exclude = function () {
+Builder.prototype.exclude = function() {
     this._isExclude = true;
     return this;
 };
 
-Builder.prototype.build = function (root) {
+Builder.prototype.build = function(root) {
     if (Array.isArray(this._pattern))
-        return this._pattern.map(function (pattern) {
-            return this._buildPattern(pattern, root)
+        return this._pattern.map(function(pattern) {
+            return this._buildPatternItem(root, pattern)
         }, this);
-    return this._buildPattern(this._pattern, root)
+
+    return this._buildPattern(root);
 };
 
-Builder.prototype._buildPattern = function (pattern, root) {
-    if (typeof root === 'undefined')
-        root = '';
-    var result = path.join('' + root, pattern + '.' + this._extension);
+Builder.prototype._buildPattern = function(root) {
+    return this._buildPatternItem(root, this._pattern);
+};
+
+Builder.prototype._buildPatternItem = function(root, pattern) {
+    var result = CURRENT_DIR + path.join(root, this._buildPatternWithExtension(pattern));
     if (this._isExclude)
         result = '!' + result;
     return result;
+}
+
+Builder.prototype._buildPatternWithExtension = function(pattern) {
+    return pattern + '.' + this._extension;
 };
 
-var BuilderFactory = function (pattern, ext) {
+var BuilderFactory = function(pattern, ext) {
     return new Builder(pattern, ext);
 };
 
 BuilderFactory.Type = Builder;
 
-BuilderFactory.allOfType = function (ext) {
-    return new Builder('/*', ext);
+BuilderFactory.allOfType = function(ext) {
+    return new Builder('*', ext);
 };
 
-BuilderFactory.deepAllOfType = function (ext) {
-    return new Builder('/**/*', ext);
+BuilderFactory.deepAllOfType = function(ext) {
+    return new Builder('**/*', ext);
 };
 
 module.exports = BuilderFactory;
-
