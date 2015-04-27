6f63fb71a8cfacb5c09d20d639c273168817d471
Borys Ihnatyev <paur.link@gmail.com>
2015-04-27 21:19:55 +0300

    [Automation] - Replace gulp legacy watch with watchify.

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -8,12 +8,24 @@ gulp.task('server', ['watch'], function () {
     nodemon({
         watch: [config.paths.server._root],
         script: config.paths.server.app,
-        ignore: [config.patterns.dist.all],
+        ignore: [config.paths.dist._root],
         ext: 'js'
+    }).on('restart', function (files) {
+        console.log(files);
     });
 
     browserSync.init({
         proxy: "localhost:" + serverConfig.port,
+        port: +serverConfig.port + 1,
+        ui: {
+            port: +serverConfig.port + 2
+        }
+    });
+});
+
+gulp.task('server:static', ['watch'], function () {
+    browserSync.init({
+        server: config.paths.dist._root,
         port: +serverConfig.port + 1,
         ui: {
             port: +serverConfig.port + 2
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     config = require('../config');
 
-gulp.task('watch', ['build'], function () {
+gulp.task('watch', ['watch:js', 'build'], function () {
     gulp.watch([
             config.patterns.src.html.pages,
             config.patterns.src.html.pageComponents
@@ -9,5 +9,4 @@ gulp.task('watch', ['build'], function () {
     );
     gulp.watch(config.patterns.src.html.views, ['build:html:views']);
     gulp.watch(config.patterns.src.scss, ['build:scss_light']);
-    gulp.watch(config.patterns.src.js, ['build:js']);
 });
