03da093966b66003fe3d39277d5ef6504a1f6564
Borys Ihnatyev <paur.link@gmail.com>
2015-05-09 00:47:36 +0300

    [Js app structure] - Moved menu, header modules ( from: ./common -> ./modules). - At point of main module they are common, becouse apears in every 'sub-view' of main-view (for now), unlike task list. Taks list apears not in all 'sub-views' of main-view.

--- client/js/app/main/modules/index.js
+++ client/js/app/main/modules/index.js
@@ -1,4 +1,9 @@
 module.exports = [
+    // main view-components
+    require('./menu'),
+    require('./header'),
+    
+    // main view-modules :
     require('./today'),
     require('./week'),
     require('./project')
