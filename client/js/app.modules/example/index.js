2d8d2e5a2ab8ccf89fa8cc5bea02fe1ff756377c
Borys Ihnatyev <paur.link@gmail.com>
2015-05-08 14:42:06 +0300

    [Js app structure] - Move modules declaration into index files

--- client/js/app.modules/example/index.js
+++ client/js/app.modules/example/index.js
@@ -1,1 +1,12 @@
-module.exports = require('./example.module.js').name;
+var angular = require('angular');
+
+var exampleModule = angular.module('example', [require('../../shared.modules/greet')])
+    .config(function ($stateProvider) {
+        $stateProvider.state('example', {
+            url: '/example',
+            templateUrl: '/views' + __dirname + '/example.view.html',
+            controller: require('./example.controller.js')
+        });
+    });
+
+module.exports = exampleModule.name;
--- client/js/app.modules/main/index.js
+++ client/js/app.modules/main/index.js
@@ -1,1 +1,8 @@
-module.exports = require('./main.module.js').name;
+var angular = require('angular');
+
+var mainModule = angular.module('mainModule', [])
+    .controller('mainCtrl', require('./main.controller.js'))
+    .directive("ngHeaderBlock", require('./ng-header-block.directive.js'))
+    .factory('mainPageInfo', require('./main-page-info.factory.js'));
+
+module.exports = mainModule.name;
