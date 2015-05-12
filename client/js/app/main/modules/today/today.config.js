a2b9c3857a31bb3b33f9bc9e8add6d6a6850f9ab
Borys Ihnatyev <paur.link@gmail.com>
2015-05-12 22:02:53 +0300

    [Js app structure] - Make default state - main.today.

--- client/js/app/main/modules/today/today.config.js
+++ client/js/app/main/modules/today/today.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = function ($stateProvider) {
     $stateProvider.state('main.today', {
-        url: '/today',
+        url: '',
         templateUrl: {
             root: __dirname,
             name: 'today.view.html'
