1bce971ab4d00d5c8420fabf6810b48e2015b236
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-15 21:38:29 +0300

    Add project model

---
+++ server/models/project.model.js
@@ -0,0 +1,18 @@
+// Get connection to DataBase
+var db = require('./../bootstrap/db.connection.js');
+
+// Schema for project-list
+var projectListSchema = db.mongoose.Schema({
+    name: String,
+    color: String,
+    //tasks: [{
+    //    name: String,
+    //    date: String,
+    //    notificationTime: String,
+    //    accessTime: String
+    //}],
+    taskCount: Number
+});
+
+// Export model to needed controller in Do-While/server/routes
+module.exports = db.connection.model('ProjectListSchema', projectListSchema);
