b428619c307339e1e79914e8cca5817117f44144
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-15 23:56:10 +0300

    Update requires

--- client/js/app/main/modules/menu/index.js
+++ client/js/app/main/modules/menu/index.js
@@ -1,7 +1,8 @@
 var angular = require('angular');
 
 var menuModule = angular.module('menuModule', [
-    require('./projectList')
+    require('./projectList'),
+    require('./createProject')
 ])
     .directive('dwMenu', require('./menu.directive'))
     .service('menuService', require('./menu.service'));
