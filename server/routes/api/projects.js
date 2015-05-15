67f8828c9ba9f89cf839633b99b41b3a6f4141b7
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-15 21:41:01 +0300

    Add projects route

---
+++ server/routes/api/projects.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    router = express.Router(),
+    Project = require('../../models/project.model');
+
+// This route for getting project data to the client
+router.get('/', function(req, res) {
+
+    Project.find({}, function(err, result) {
+        if(!err){
+            res.json({items: result});
+        }
+        console.log('There is some problems with getting data from DB!!!'.toUpperCase());
+    });
+
+});
+
+// This route for getting project data from client and saving them to DB
+router.post('/add', function(req, res){
+
+    console.log(req.body.name + '---' + req.body.color);
+
+    var newProject = Project({
+        name: req.body.name,
+        color: req.body.color,
+        taskCount: Math.floor((Math.random() * 10) + 1)    // This is for demo (random task count)
+    });
+
+    newProject.save(function(err){
+        if(err){
+            console.log('There is some problem with saving data to DB!!!'.toUpperCase());
+        }
+    });
+});
+
+module.exports = router;
