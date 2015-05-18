2b7851b656d202448be09d6763475dd0f33771dc
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-18 23:57:56 +0300

    Delete create project module from previous location

--- client/js/app/main/modules/menu/createProject/index.js
+++
@@ -1,8 +0,0 @@
-var angular = require('angular');
-
-var createModule = angular.module('createModule', [])
-    .controller('createCtrl', require('./createProject.controller'))
-    .service('createService', require('./createProject.service'))
-    .config(require('./createProject.config'));
-
-module.exports = createModule.name;
